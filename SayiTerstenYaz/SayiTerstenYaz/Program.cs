8c9b9e54f19ba6cf5a1135cf4123de6f9cf49726
omer kor <omerkorr@gmail.com>
2020-02-13 10:40:44 +0300

    last

--- SayiTerstenYaz/SayiTerstenYaz/Program.cs
+++ SayiTerstenYaz/SayiTerstenYaz/Program.cs
@@ -10,6 +10,21 @@ namespace SayiTerstenYaz
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Bir sayı giriniz");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                int tersten_sayi = 0;
+                var Fsayi = sayi;
+                do
+                {
+                    tersten_sayi = tersten_sayi * 10; // bir sonraki basmağı sıfır yapıp sayıdan gelen basamağı eklemek için(3)
+                    tersten_sayi = tersten_sayi + sayi % 10; //sayının bir basamağını başa almak için (1)
+                    sayi = sayi / 10; // sayının birler basamaığından kurtulmak için (2)
+                } while (sayi!=0);
+                Console.WriteLine("({0}) sayısının tersten yazlışı; \n({1}) şeklindedir",Fsayi,tersten_sayi);
+                Console.Read();
+            }
         }
     }
 }
